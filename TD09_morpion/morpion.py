1ff1eb880265ce5638483d478d14bdcb4349b560
C. Baskevitch <cbask@laposte.net>
2024-04-03 21:21:26 +0200

    ajout TD09 morpion

---
+++ TD09_morpion/morpion.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+
+# initialisation variables globales
+CANVAS_WIDTH = 600
+CANVAS_HEIGHT = 600
+largeur_case = CANVAS_WIDTH // 3
+hauteur_case = CANVAS_HEIGHT // 3
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    """
+    Code principal du programme qui initialise l'interface graphique et cree une nouvelle partie
+    """
+
+
